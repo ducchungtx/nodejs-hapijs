a438e0c327802a54e7a81632e2db96c4f9a0348f
Chung Nguyen <zrmedia9@gmail.com>
2018-02-07 22:18:21 +0700

    add plugin for hapi

--- server.js
+++ server.js
@@ -2,30 +2,47 @@ const hapi = require('hapi');
 
 const server = new hapi.Server();
 
-server.connection({ host: '127.0.0.1', port: '3000' });
+server.connection({host: '127.0.0.1', port: '3000'});
 
 
 server.route({
-    path: '/',
-    method: 'GET',
-    handler(req, reply) {
-        reply('Welcome to HapiJs course!!');
-    }
+   path: '/',
+   method: 'GET',
+   handler(req,reply){
+       reply('Welcome to HapiJs course!!');
+   }
 });
 
 server.route({
     path: '/companies',
-    method: 'GET',
-    handler(req, reply) {
+    method:'GET',
+    handler(req,reply){
         reply('Welcome to Companies route');
     }
 });
 
 
-
-server.start(err => {
-    if (err) {
+server.register({
+    register: require('good'),
+    options:{
+        reporters: {
+            myConsoleReporter: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{log: '*', response: '*'}]
+            }, {
+                module: 'good-console'
+            }, 'stdout'],
+        }
+    }
+},err => {
+    if(err){
         throw err;
     }
-    console.log(`Server Running at PORT ${server.info.port}`);
+    server.start(err => {
+        if(err){
+            throw err;
+        }
+        console.log(`Server Running at PORT ${server.info.port}`);
+    });
 });
